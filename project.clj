139ee248ead69d9d6c90f4491ccae0a3b648e283
Walter Pelissero <wcp@book.home.lan>
2022-03-30 16:12:35 +0200

    Add project url

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject fourtytoo/clj-evohome "0.1.0-SNAPSHOT"
   :description "A simple Clojure interface to Honeywell EVO Home"
-  :url "http://example.com/FIXME"
+  :url "http://github.com/fourtytoo/clj-evohome"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.3"]
