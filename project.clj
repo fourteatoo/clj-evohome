bbc5771dfdbf35188f9ad0e72fb8d6c782e185ee
Walter Pelissero <wcp@book.home.lan>
2025-06-14 14:13:53 +0200

    Update ancient artifacts

--- project.clj
+++ project.clj
@@ -3,9 +3,9 @@
   :url "http://github.com/fourteatoo/clj-evohome"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.10.3"]
-                 [clojure.java-time "0.3.3"]
-                 [clj-http "3.12.3"]
-                 [cheshire "5.10.1"]
-                 [camel-snake-kebab "0.4.2"]]
+  :dependencies [[org.clojure/clojure "1.12.1"]
+                 [clojure.java-time "1.4.3"]
+                 [clj-http "3.13.0"]
+                 [cheshire "6.0.0"]
+                 [camel-snake-kebab "0.4.3"]]
   :repl-options {:init-ns fourteatoo.clj-evohome.api2})
