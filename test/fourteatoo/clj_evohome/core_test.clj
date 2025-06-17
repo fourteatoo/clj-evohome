6af3e67d03f0395ec17f52bf1e0bd45ff40ac7a6
Walter Pelissero <walter@pelissero.de>
2025-06-17 18:44:00 +0200

    Add test dir

---
+++ test/fourteatoo/clj_evohome/core_test.clj
@@ -0,0 +1,8 @@
+(ns fourteatoo.clj-evohome.core-test
+  (:require
+   [clojure.test :refer :all]
+   [fourteatoo.clj-evohome.api2 :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
