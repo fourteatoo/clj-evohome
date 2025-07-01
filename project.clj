b44d9f75e9d7bee72e29c6be382ea6532c793c89
Walter Pelissero <walter@pelissero.de>
2025-07-01 22:59:37 +0200

    Version 1.0.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject io.github.fourteatoo/clj-evohome "1.0.0-SNAPSHOT"
+(defproject io.github.fourteatoo/clj-evohome "1.0.0"
   :description "A simple Clojure interface to Honeywell EVO Home"
   :url "http://github.com/fourteatoo/clj-evohome"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
